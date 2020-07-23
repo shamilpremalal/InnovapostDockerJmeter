1aa61146703bada28f026b166b71a0011972608c
Shamil Premalal <spremalal@deloitte.com>
2020-07-23 16:55:43 -0400

    Clean Up

--- Docker/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-JMETER_VERSION="5.3"
-
-# Example build line
-# --build-arg IMAGE_TIMEZONE="America/Toronto"
-docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} -t "innovapost/jmeter:${JMETER_VERSION}" .
